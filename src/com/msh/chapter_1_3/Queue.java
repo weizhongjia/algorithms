866004c1ed5e918689f5220e2213fd894011df27
weizhongjia <948913886@qq.com>
2018-07-16 21:10:03 +0800

    1.3.3

---
+++ src/com/msh/chapter_1_3/Queue.java
@@ -0,0 +1,45 @@
+package com.msh.chapter_1_3;
+
+/**
+ * 出栈操作书中的代码有问题。isEmpty方法如果使用N==0判断的话，需要先N--再判断是否要将last改为null
+ */
+public class Queue<Item> {
+    private Node first;
+    private Node last;
+    private int N;
+    private class Node {
+        Item item;
+        Node next;
+    }
+
+    public void enqueue (Item item) {
+        Node oldLast = last;
+        last = new Node();
+        last.item = item;
+        last.next = null;
+        if (isEmpty()) {
+            first = last;
+        } else {
+            oldLast.next = last;
+        }
+        N++;
+    }
+
+    public Item dequeue () {
+        Item item = first.item;
+        first = first.next;
+        N--;
+        if (isEmpty()) {
+            last = null;
+        }
+        return item;
+    }
+
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    public int size() {
+        return N;
+    }
+}
